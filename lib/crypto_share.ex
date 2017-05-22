2996839ffa08030a6f5d8b5bb24dcefaed1afde3
Simon Žlender <simon.zlender@gmail.com>
2017-05-22 02:44:12 +0200

    Remove generated code

--- lib/crypto_share.ex
+++ lib/crypto_share.ex
@@ -1,18 +1,3 @@
 defmodule CryptoShare do
-  @moduledoc """
-  Documentation for CryptoShare.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CryptoShare.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
